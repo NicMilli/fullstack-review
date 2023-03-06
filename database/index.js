64f97f6194089739707c68d4b3760659071066fc
NicMilli <nicbass2@gmail.com>
2023-03-06 11:12:32 -0800

    Dependencies installed and database schema started

--- database/index.js
+++ database/index.js
@@ -1,11 +1,35 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to MongoDB successfully');
 });
 
+let repoSchema = mongoose.Schema({
+  username: String,
+  title: String,
+  description: String,
+  starCount: Number,
+  forkCount: Number,
+  url: String
+});
+
+repoSchema.methods.order = function() {
+  this.repos.filter((a, b) => {
+    if (a.stars > b.stars) {
+      return -1;
+    } else if (a.stars > b.stars) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+};
+
 let Repo = mongoose.model('Repo', repoSchema);
+Repo.order();
 
 let save = (/* TODO */) => {
   // TODO: Your code here
