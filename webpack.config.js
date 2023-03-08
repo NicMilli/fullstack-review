bfaec012d52c72b1921bdcce23dec1df5e7a004b
NicMilli <nicbass2@gmail.com>
2023-03-08 08:01:54 -0800

    webpack reverted

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var DIST_DIR = path.join(__dirname, "/client/dist");
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: "bundle.js.LICENSE.txt",
+    filename: "bundle.js",
     path: DIST_DIR,
   },
   module: {
